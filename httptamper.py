12556e2084604897c17ac2647483f0c46c3ce6eb
Thomas 'thms' M <thms@localhost>
2019-03-14 14:37:23 +0100

    update

--- httptamper.py
+++ httptamper.py
@@ -1,7 +1,9 @@
 import requests
 
 verbs = ["GET", "PUT", "HEAD", "OPTIONS", "POST", "TRACE", "DELETE", "CONNECT"]
-url = "http://challenge01.root-me.org/realiste/ch3/admin/"
+#url = "http://challenge01.root-me.org/realiste/ch3/admin/"
+#url = "http://challenge01.root-me.org/realiste/ch7/hackers.php?page=exploits"
+url = "http://challenge01.root-me.org/realiste/ch7/log/log.php"
 
 for verb in verbs:
     print "-----------------------"
